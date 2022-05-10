02dcecc23c5a895841c21a641bb53118743163ba
Breno Trengrouse <brenot@gmail.com>
2022-05-10 10:40:17 -0300

    styling HOME 005

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -3,16 +3,20 @@
     <!-- Footer -->
     <footer class="bg-dark text-center text-white mt-auto">
         <!-- Grid container -->
-        <div class="container p-4">
+        <div class="container ">
             <!-- Section: Links -->
             <section class="">
                 <!--Grid row-->
                 <div class="row">
 
                     <div class="logo-footer mt-5">
-                        <a href="http://www.palaciotiradentes.rj.gov.br/"><img src="http://www.palaciotiradentes.rj.gov.br/wp-content/uploads/2016/04/tt-acompanhe.png" alt="footer_logo"></a></div>
+                        <h3 class="text-uppercase">
+                            Acompanhe Nossas Redes
+                        </h3>
 
-                    <ul id="footer-menu" class="menu-footer"><li id="menu-item-38" class="facebook menu-item menu-item-type-custom menu-item-object-custom menu-item-38"><a target="_blank" href="https://www.facebook.com/assembleiaRJ">Facebook</a></li>
+                    </div>
+
+                    <ul id="footer-menu" class="menu-footer mt-3 mb-5"><li id="menu-item-38" class="facebook menu-item menu-item-type-custom menu-item-object-custom menu-item-38"><a target="_blank" href="https://www.facebook.com/assembleiaRJ">Facebook</a></li>
                         <li id="menu-item-39" class="instagram menu-item menu-item-type-custom menu-item-object-custom menu-item-39"><a target="_blank" href="https://www.instagram.com/instalerj/">Instagram</a></li>
                         <li id="menu-item-40" class="twitter menu-item menu-item-type-custom menu-item-object-custom menu-item-40"><a target="_blank" href="https://twitter.com/alerj">Twitter</a></li>
                         <li id="menu-item-41" class="youtube menu-item menu-item-type-custom menu-item-object-custom menu-item-41"><a target="_blank" href="https://www.youtube.com/user/dcsalerj">Youtube</a></li>
