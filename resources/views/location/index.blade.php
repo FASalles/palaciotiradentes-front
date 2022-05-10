3d20b3b8c77fa96760f7edb281a645f1a531cfaf
Breno Trengrouse <brenot@gmail.com>
2022-05-10 10:56:19 -0300

    styling location

--- resources/views/location/index.blade.php
+++ resources/views/location/index.blade.php
@@ -8,7 +8,7 @@
             </div>
             <div class="tr-content-title-banner">
                 <div class="header-title-wrap">
-                    <h2 class="header-title" style="color: #ffffff">Mapa de Localização</h2>
+                    <h2 class="header-title" style="color: #ffffff">Como Chegar</h2>
                 </div>
 
                 <div class="header-breadcrumb-wrap color-changer1" data-color="#999999" data-c-target="span, li">
@@ -21,7 +21,7 @@
 
                             </li>
                             <li class="trail-item trail-end">
-                                <span>Mapa de Localização</span>
+                                <span>Como Chegar</span>
 
                             </li>
                         </ul>
@@ -33,50 +33,21 @@
         <div class="container page-content">
             <div class="row mb-2 text-center">
                 <div class="col-md-12 page-title">
-                    <h2>PALÁCIO TIRADENTES</h2>
-                    <h6>Através da História</h6>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-sm">
-                    <div class="wpb_wrapper">
-                        <p>O local onde está hoje o Palácio Tiradentes é um sítio histórico que guarda – desde os tempos do Brasil Colônia – grande parte da memória política do Brasil. O primeiro edifício ali construído data de 1640 e serviu para abrigar os primeiros três vereadores eleitos. O voto era indireto e o durava apenas um ano. Cabia a eles cuidar da cidade e das finanças públicas. Todo o dinheiro arrecadado ficava guardado em um cofre chamado “burra”, que só podia ser aberto por três chaves: cada uma ficava com um vereador.</p>
-                        <p>No andar abaixo do cofre público ficava a cadeia da cidade que ficou conhecida como a “cadeia velha”. Foi lá onde o alferes Joaquim José da Silva Xavier – Tiradentes, ficou preso durante três anos antes de ser enforcado em 21 de abril de 1792.</p>
-                        <p>Quando a Coroa portuguesa aqui chegou, fugindo da invasão napoleônica, em 1808, e se instalou no Paço Imperial ao lado da “cadeia velha”, o Parlamento, que já contava com um número maior de representantes, foi deslocado para outro prédio deixando o paço receber todos os empregados da Corte Real.</p>
-                    </div>
-                </div>
-                <div class="col-sm">
-                    <div class="wpb_wrapper">
-                        <p>A seguir, de volta a um dos andares da velha cadeia, a<u></u>&nbsp;Câmara Imperial começara a protagonizar fatos memoráveis, como exemplo a assinatura da Lei Áurea, em 1888.<u></u>&nbsp;<u></u></p>
-                        <p>Em 1922, já em situação precária, a edificação é demolida para dar lugar a um grande palácio, cuja arquitetura lembraria muito o&nbsp;<em>Grand Palais</em>&nbsp;de Paris. Projetado em estilo eclético por Archimedes Memória e Francisco Couchet o Palácio Tiradentes é inaugurado em 6 de maio de 1926, e, passa a abrigar a Câmara federal que ali funcionou de 1926 a 1960, tendo recebido para&nbsp; a posse todos os presidentes do período- de Washington Luiz a Juscelino Kubitscheck.<u></u>&nbsp;<u></u></p>
-                        <p>De 1937 a 1945, durante o Estado Novo, o Parlamento foi fechado pelo presidente Getúlio Vargas e no Palácio Tiradentes passa a funcionar o Ministério da Justiça e o Departamento de Imprensa e Propaganda (DIP), órgão de censura do regime.<u></u></p>
-                    </div>
-                </div>
-                <div class="col-sm">
-                    <div class="wpb_wrapper">
-                        <p>Em 1960, quando da transferência da capital do país para Brasília, o Palácio Tiradentes abriga a Assembleia Legislativa do Estado da Guanabara (ALEG) que 15 anos depois, com a fusão dos estados da Guanabara e do Rio de Janeiro, passa a se chamar Assembleia Legislativa do Estado do Rio de Janeiro (Alerj).<u></u>&nbsp;<u></u></p>
-                        <p>Hoje em dia, detalhes dessa rica história pode ser conhecida na exposição permanente “Palácio Tiradentes: Lugar de Memória do Parlamento Brasileiro” onde o visitante pode acompanhar passo a passo os acontecimentos que marcaram o surgimento da&nbsp;suntuosa&nbsp;edificação&nbsp;e&nbsp; descobrir que não está apenas caminhando por um importante marco arquitetônico carioca. O visitante estará sim… conhecendo o mais importante referencial da democracia brasileira.<u></u>&nbsp;<u></u></p>
-                        <p>As escadarias do Palácio Tiradentes não só abrigaram manifestações políticas e de protestos. Ao longo dos últimos anos, desde a concepção da visita guiada (1998), esteve à disposição do público como palco para espetáculos culturais com danças, arte dramática, shows de música e até desfiles de moda.<u></u>&nbsp;<u></u></p>
-                        <p>Do mesmo modo, suas galerias, corredores, o plenário e o salão nobre estão abertos aos visitantes, turistas – nacionais e estrangeiros –&nbsp;que encontram no ali um espaço alternativo para a expressão da arte e do entretenimento</p>
-                        <p>Visite-o</p>
-                    </div>
+                   {{-- <h2>PALÁCIO TIRADENTES</h2>
+                    <h6>Através da História</h6>--}}
+                        <h1 class="titulo" style="text-align: center;">PALÁCIO TIRADENTES</h1>
+                        <p style="text-align: center;">Rua Primeiro de março, s/n – Praça XV – Rio de Janeiro<br>
+                            CEP 20010-090 &nbsp;&nbsp; Telefone +55 (21) 2588-1000 &nbsp;&nbsp; Fax +55 (21) 2588-1516
+                        </p>
                 </div>
             </div>
         </div>
 
-        <div class="container-fluid mt-5">
+        <div class="container-fluid mb-5">
             <div class="row">
-                <div class="bg-light bg-history text-secondary px-4 py-5 text-center">
-                    <div class="py-5">
-                        <h1 class="display-5 fw-bold mt-3 ">Entre no Túnel do Tempo</h1>
-                        <div class="col-lg-8 mx-auto mb-5">
-                            <p class="fs-5 mb-5 mt-4">Desde os tempos do Brasil Colônia, o Palácio Tiradentes é um lugar histórico que guarda um grande pedaço da memória política do Brasil.</p>
-                            <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                                <button type="button" class="btn btn-primary btn-lg px-4 me-sm-3 fw-bold">Conheça a Linha do Tempo <i class="ms-3 fa-solid fa-arrow-right"></i></button>
-                            </div>
-                        </div>
-                    </div>
+                <div class="col-12">
+                    <img src="images/map.jpg" class="img-fluid">
+
                 </div>
             </div>
         </div>
