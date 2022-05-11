458bdd465bef3a53c95af2f0021f5bce6c4a0e78
BMasquio <brunomasquio@gmail.com>
2022-05-11 14:12:57 -0300

    Google analytics - Issue #11

---
+++ resources/views/layouts/partials/google-analytics.blade.php
@@ -0,0 +1,9 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-TE8GN0NH2D"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-TE8GN0NH2D');
+</script>
