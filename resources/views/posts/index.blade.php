3898994b51b3b2a478f33e16f3eebb49db4fd570
marcoscoe <marcoscoe9@gmail.com>
2022-05-11 19:52:59 +0000

    Notícia blade

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,6 +2,33 @@
 
 @section('main')
 <section class="">
+
+<div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>
+                    Notícias em destaque
+                </h1>
+            </div>
+        </div>
+
+        @foreach($posts->where('featured','true') as $post)
+        <div class="col-md-6">
+            <div class="row mt-2 g-0 border-bottom overflow-hidden pb-2">
+                <div class="col p-4">
+                    <div class="mb-1 text-muted">{{ $post->publish_start_date->format('d/m/Y') }}</div>
+                    <h3 class="mb-2">{{$post->title}}</h3>
+                    <p class="card-text mb-auto"> {{ $post->description}} </p>
+                    <a href="#" class="btn btn-bricks mt-2">Leia Mais</a>
+                </div>
+                <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]" src="{{$post->image('cover', 'mobile')}}" data-holder-rendered="true" style="width: 200px; height: 250px;">
+            </div>
+        </div>
+        @endforeach
+
+    </div>
+
+
     <div class="container">
         <div class="row">
             <div class="col-12">
@@ -17,7 +44,7 @@
                 <div class="col p-4">
                     <div class="mb-1 text-muted">{{ $post->publish_start_date->format('d/m/Y') }}</div>
                     <h3 class="mb-2">{{$post->title}}</h3>
-                    <span class="card-text mb-auto"> {!! $post->subject !!} </span>
+                    <p class="card-text mb-auto"> {{ $post->description}} </p>
                     <a href="#" class="btn btn-bricks mt-2">Leia Mais</a>
                 </div>
                 <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]" src="{{$post->image('cover', 'mobile')}}" data-holder-rendered="true" style="width: 200px; height: 250px;">
