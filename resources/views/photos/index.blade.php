b0fba5c99ab94d52e3f55207b3847deb12e38243
Breno Trengrouse <brenot@gmail.com>
2022-05-17 11:13:34 -0300

    videos + photos layouts

--- resources/views/photos/index.blade.php
+++ resources/views/photos/index.blade.php
@@ -37,7 +37,7 @@
 
         </div>
 
-        <div class="album py-5">
+        <div class="album pb-5">
             <div class="container">
 
                 <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
