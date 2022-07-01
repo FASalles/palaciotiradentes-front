8737c94154350f54297bf3ebdd6a6d053e5d41c5
Breno Trengrouse <brenot@gmail.com>
2022-07-01 14:06:50 -0300

    video SHOW link embed

--- resources/views/videos/index.blade.php
+++ resources/views/videos/index.blade.php
@@ -1,3 +1,4 @@
+
 @extends('layouts.layout')
 @section('main')
 
@@ -8,20 +9,20 @@
             </div>
             <div class="tr-content-title-banner">
                 <div class="header-title-wrap">
-                    <h2 class="header-title" style="color: #ffffff">Galeria de Vídeos/h2>
+                    <h2 class="header-title" style="color: #ffffff">Galeria de Vídeos</h2>
                 </div>
 
                 <div class="header-breadcrumb-wrap color-changer1" data-color="#999999" data-c-target="span, li">
                     <nav aria-label="Breadcrumbs" class="breadcrumb-trail breadcrumbs">
                         <ul class="trail-items" itemscope="" itemtype="http://schema.org/BreadcrumbList">
                             <li class="trail-item trail-begin">
-                                <a href="http://www.palaciotiradentes.rj.gov.br" rel="home">
+                                <a href="//www.palaciotiradentes.rj.gov.br" rel="home">
                                     <span>Home</span>
                                 </a>
 
                             </li>
                             <li class="trail-item trail-end">
-                                <span>Galeria de Vídeos/span>
+                                <span>Galeria de Vídeos</span>
 
                             </li>
                         </ul>
@@ -30,24 +31,54 @@
             </div>
         </div>
 
+
         <div class="container page-content">
             <div class="row mb-2 text-center">
                 <div class="col-md-12 page-title">
                     <h2>
-                        Palácio Tiradentes na mídia
+                        UM PALÁCIO QUE FAZ HISTÓRIA!
                     </h2>
                     <h6>
-                        Seja por sua história ou pelos eventos realizados nele, o Palácio foi notícia. Confira!
+                        Palco de inúmeras discussões políticas e local de trabalho de milhares de pessoas
+                        ao longo de seus 90 anos, não faltam histórias
+                        de quem caminha ou já passou pelo movimentado Palácio Tiradentes.
+                        Confira os depoimentos e documentários de quem participou dessa trajetória.
                     </h6>
                 </div>
             </div>
 
-            <div class="row">
-                <div class="col-12">
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
 
-                 </div>
+                @foreach($videos as $video)
+                    <div class="col">
+                        <div class="card shadow-sm">
+                            <a href="{{route('videos.show', ['slug'=> $video->slug])}}"><img src="{{$video->image('cover', 'default')}}" class="card-img-top" alt="..."></a>
+
+                            <div class="card-body">
+                                <h5>{{$video->title}}</h5>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+                {{--
+                     @foreach($videos as $video)
+                     <div class="col-md-3">
+                         <div class="card" style="width: 18rem;">
+                             <a href="{{route('videos.show', ['slug'=> $video->slug])}}"><img src="{{$video->image('cover', 'mobile')}}" class="card-img-top" alt="..."></a>
+                             <div class="card-body">
+                                 <h5>{{$video->title}}</h5>
+                             </div>
+                         </div>
+                     </div>
+                     @endforeach--}}
+
             </div>
+
+
         </div>
+
+
+
 
         <div class="container-fluid mt-5">
             <div class="row">
