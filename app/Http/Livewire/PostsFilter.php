26a0d1c6664110832dd1cbe6385fa311a45597dc
marcoscoe <marcoscoe9@gmail.com>
2022-09-16 13:30:36 +0000

    pagination

--- app/Http/Livewire/PostsFilter.php
+++ app/Http/Livewire/PostsFilter.php
@@ -3,7 +3,6 @@
 namespace App\Http\Livewire;
 
 use App\Twill\Capsules\Posts\Models\Post;
-use App\Twill\Capsules\Posts\Repositories\PostRepository;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -12,17 +11,20 @@ class PostsFilter extends Component
 
     use WithPagination;
 
-    public $search = null;
+    public $search = '';
 
-
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+    
     public function render()
     {
 
         $posts1 = Post::published()->orderByDesc('publish_start_date');
 
         $posts1->when($this->search, function($queryBuilder) {
-            return $queryBuilder->where('title', 'ILIKE', '%'. $this->search . '%')
-            ->orWhere('description','ILIKE', '%'. $this->search . '%');
+            return $queryBuilder->where('title', 'ILIKE', '%'. $this->search . '%');
         });
        
         return view('livewire.posts-filter')->with(['posts1' => $posts1->paginate(5)]);
