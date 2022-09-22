148da4bd5f78a4c93c47e4c6bdacd60e75552cf8
marcoscoe <marcoscoe9@gmail.com>
2022-09-22 17:19:36 +0000

    migration galeria de vídeos

--- database/migrations/2022_09_13_174000_migration_galeria_de_fotos.php
+++ database/migrations/2022_09_13_174000_migration_galeria_de_fotos.php
@@ -13,118 +13,102 @@ return new class extends Migration
      */
     public function up()
     {
-        $posts = DB::connection('mysql')->select('
+        $posts = DB::connection('mysql')->select(
+            '
         SELECT * from wp_posts p
         where p.post_status = "publish"
         and p.post_parent = 817; '
         );
 
-       // dump ($posts);
+        // dump ($posts);
 
-        foreach($posts as $post) {
-            $post_contents =explode("[", $post->post_content);
+        foreach ($posts as $post) {
+            $post_contents = explode("[", $post->post_content);
 
             $photoTwiilId = DB::table('photos')->insertGetId(
                 array(
                     'created_at' => $post->post_date,
                     'updated_at' => $post->post_date,
                     'published' => true,
-                    'title'=>$post->post_title,
-                    'credits'=>'INSERIR CRÉDITOS',                    
-                    'publish_start_date' =>$post->post_date
-                ));
+                    'title' => $post->post_title,
+                    'credits' => 'INSERIR CRÉDITOS',
+                    'publish_start_date' => $post->post_date
+                )
+            );
 
-                DB::table('photo_slugs')->insertGetId(
-                    array(
-                        'created_at' => $post->post_date,
-                        'updated_at' => $post->post_date,
-                        'active' => true,
-                        'slug'=>$post->post_name,                                          
-                       'photo_id'=>$photoTwiilId,
-                       'locale'=>'en'
-                    ));
-            foreach($post_contents as $content) {
+            DB::table('photo_slugs')->insertGetId(
+                array(
+                    'created_at' => $post->post_date,
+                    'updated_at' => $post->post_date,
+                    'active' => true,
+                    'slug' => $post->post_name,
+                    'photo_id' => $photoTwiilId,
+                    'locale' => 'en'
+                )
+            );
+            foreach ($post_contents as $content) {
 
                 // preg_replace('/ess_grid alias="(.*)\"]/', '$0 --> $1', $content);
                 // dump ($1);
-                $alias = explode('-->',preg_replace('/ess_grid alias="(.*)\"]/', '$0 --> $1', $content));
-                if(count($alias) > 1){
+                $alias = explode('-->', preg_replace('/ess_grid alias="(.*)\"]/', '$0 --> $1', $content));
+                if (count($alias) > 1) {
                     $titulo = trim($alias[1]);
-                        dump("Encontrando Galeria: " . $titulo);
-                    $egGrid = DB::connection('mysql')->select('SELECT * FROM wp_eg_grids wpeg where wpeg.handle = "'. $titulo .'"');
+                    dump("Encontrando Galeria: " . $titulo);
+                    $egGrid = DB::connection('mysql')->select('SELECT * FROM wp_eg_grids wpeg where wpeg.handle = "' . $titulo . '"');
 
-//                    dd($egGrid);
+                    //                    dd($egGrid);
 
                     $layers = json_decode($egGrid[0]->layers);
 
                     $ids = array();
-                    dump("Obtendo os ids das fotos" );
-                    foreach($layers as $layer){            
-                        $layer =json_decode($layer,true);
-                        
-                        array_push($ids,$layer["custom-image"]);
+                    dump("Obtendo os ids das fotos");
+                    foreach ($layers as $layer) {
+                        $layer = json_decode($layer, true);
+
+                        array_push($ids, $layer["custom-image"]);
                         dump($layer["custom-image"]);
-                    }                                
+                    }
                     dump("Obtendo as fotos do wp_posts");
-                    $WPphotos = DB::connection('mysql')->select('
+                    $WPphotos = DB::connection('mysql')->select(
+                        '
                     SELECT * from wp_posts p
-                    where p.id in ( ' . implode(",",$ids) . ')'
+                    where p.id in ( ' . implode(",", $ids) . ')'
                     );
 
-                    foreach($WPphotos as $wpphoto){
+                    foreach ($WPphotos as $wpphoto) {
 
                         dump('Inserindo: ' . $wpphoto->post_name);
-                       $mediaId = DB::table('medias')->insertGetId(
+                        $mediaId = DB::table('medias')->insertGetId(
                             array(
                                 'created_at' => $wpphoto->post_date,
                                 'updated_at' => $wpphoto->post_date,
-                                'uuid' => preg_replace('/http:\/\/www.palaciotiradentes.rj.gov.br\/wp-content\/uploads\//', '$1',$wpphoto->guid),
-                                'alt_text'=>$wpphoto->post_title,
-                                'width'=>0,
-                                'height'=>0,
-                                'filename'=>$wpphoto->post_name.'.jpg'
+                                'uuid' => preg_replace('/http:\/\/www.palaciotiradentes.rj.gov.br\/wp-content\/uploads\//', '$1', $wpphoto->guid),
+                                'alt_text' => $wpphoto->post_title,
+                                'width' => 0,
+                                'height' => 0,
+                                'filename' => $wpphoto->post_name . '.jpg'
                             )
                         );
 
-                        dump('Inserindo no relacionamento com a tabela Photos ' . $photoTwiilId . ' --> '. $mediaId);
-                        
-                        DB::table('mediables')->insertGetId(
-                            array(
-                                'created_at' => $wpphoto->post_date,
-                                'updated_at' => $wpphoto->post_date,
-                                'mediable_id'=>$photoTwiilId,
-                                'mediable_type'=>'App\Models\Photo',
-                                'media_id'=>$mediaId,
-                                'crop_x'=>0,
-                                'crop_y'=>0,
-                                'crop_w'=>0,
-                                'crop_h'=>0,
-                                'role'=>'slideshow',
-                                'crop'=>'default',
-                                'ratio'=>'default',
-                                'metadatas'=>'{"caption":null,"altText":null,"video":null}',
-                                'locale'=>'en'
-                                
-                            )
-                        );
+                        dump('Inserindo no relacionamento com a tabela Photos ' . $photoTwiilId . ' --> ' . $mediaId);
 
                         DB::table('mediables')->insertGetId(
                             array(
                                 'created_at' => $wpphoto->post_date,
                                 'updated_at' => $wpphoto->post_date,
-                                'mediable_id'=>$photoTwiilId,
-                                'mediable_type'=>'App\Models\Photo',
-                                'media_id'=>$mediaId,
-                                'crop_x'=>0,
-                                'crop_y'=>0,
-                                'crop_w'=>0,
-                                'crop_h'=>0,
-                                'role'=>'slideshow',
-                                'crop'=>'mobile',
-                                'ratio'=>'mobile',
-                                'metadatas'=>'{"caption":null,"altText":null,"video":null}',
-                                'locale'=>'en'
-                                
+                                'mediable_id' => $photoTwiilId,
+                                'mediable_type' => 'App\Models\Photo',
+                                'media_id' => $mediaId,
+                                'crop_x' => 0,
+                                'crop_y' => 0,
+                                'crop_w' => 0,
+                                'crop_h' => 0,
+                                'role' => 'slideshow',
+                                'crop' => 'default',
+                                'ratio' => 'default',
+                                'metadatas' => '{"caption":null,"altText":null,"video":null}',
+                                'locale' => 'en'
+
                             )
                         );
 
@@ -132,32 +116,45 @@ return new class extends Migration
                             array(
                                 'created_at' => $wpphoto->post_date,
                                 'updated_at' => $wpphoto->post_date,
-                                'mediable_id'=>$photoTwiilId,
-                                'mediable_type'=>'App\Models\Photo',
-                                'media_id'=>$mediaId,
-                                'crop_x'=>0,
-                                'crop_y'=>0,
-                                'crop_w'=>0,
-                                'crop_h'=>0,
-                                'role'=>'slideshow',
-                                'crop'=>'flexible',
-                                'ratio'=>'free',
-                                'metadatas'=>'{"caption":null,"altText":null,"video":null}',
-                                'locale'=>'en'
-                                
+                                'mediable_id' => $photoTwiilId,
+                                'mediable_type' => 'App\Models\Photo',
+                                'media_id' => $mediaId,
+                                'crop_x' => 0,
+                                'crop_y' => 0,
+                                'crop_w' => 0,
+                                'crop_h' => 0,
+                                'role' => 'slideshow',
+                                'crop' => 'mobile',
+                                'ratio' => 'mobile',
+                                'metadatas' => '{"caption":null,"altText":null,"video":null}',
+                                'locale' => 'en'
+
                             )
                         );
 
-                        
+                        DB::table('mediables')->insertGetId(
+                            array(
+                                'created_at' => $wpphoto->post_date,
+                                'updated_at' => $wpphoto->post_date,
+                                'mediable_id' => $photoTwiilId,
+                                'mediable_type' => 'App\Models\Photo',
+                                'media_id' => $mediaId,
+                                'crop_x' => 0,
+                                'crop_y' => 0,
+                                'crop_w' => 0,
+                                'crop_h' => 0,
+                                'role' => 'slideshow',
+                                'crop' => 'flexible',
+                                'ratio' => 'free',
+                                'metadatas' => '{"caption":null,"altText":null,"video":null}',
+                                'locale' => 'en'
+
+                            )
+                        );
                     }
-
-                   
                 }
             }
-
-
-        }        
-        
+        }
     }
 
     /**
@@ -168,7 +165,6 @@ return new class extends Migration
     public function down()
     {
         DB::table('medias')->truncate();
-        DB::table('photos')->truncate();       
-
+        DB::table('photos')->truncate();
     }
 };
--- database/migrations/2022_09_20_133528_galeria_de_videos.php
+++ database/migrations/2022_09_20_133528_galeria_de_videos.php
@@ -14,11 +14,12 @@ return new class extends Migration
     public function up()
     {
         $posts = DB::connection('mysql')->select(
-            'SELECT id, post_content, post_title, post_date from wp_posts p
+            'SELECT id, post_content, post_title, post_name, post_date from wp_posts p
         where p.post_status = "publish"
-        and p.post_parent = 883'
+        and p.post_parent = 883
+        order by post_date desc'
         );
-
+        $position = 0;
         foreach ($posts as $post) {
             dump($post_contents = explode("[", $post->post_content));
 
@@ -28,29 +29,39 @@ return new class extends Migration
                     $link = trim($vc_video[1]);
                     $link = explode('"', $link);
 
+                    preg_match_all("~\[vc_column_text[^][]*\]\s*(.+?)\[/vc_column_text]~", $post->post_content, $post->text);
+
                     $attachment = DB::connection('mysql')->select(
                         'SELECT * from wp_posts p
                     where p.post_type = "attachment"
                     and p.post_parent = ' . $post->id . ' order by id desc limit 1'
                     );
 
-                    /*
+
                     $videoID = DB::table('videos')->insertGetId(
                         array(
                             'created_at' => $post->post_date,
                             'updated_at' => $post->post_date,
                             'link' => $link[0],
                             'title' => $post->post_title,
-                            'text' => 
+                            'text' => $post->text[1] ? $post->text[1][0] : null,
+                            'position' => ++$position,
+                            'publish_start_date' => $post->post_date,
+                            'published' => true
                         )
-                    );*/
-                    //title, link, text e position
-                    //deleted_at, created_at, updated_at, published, publish_start_date e publish_end_date
+                    );
 
+                    $videoID = DB::table('video_slugs')->insertGetId(
+                        array(
+                            'slug' => $post->post_name,
+                            'video_id' => $videoID,
+                            'locale' => 'en',
+                            'active' => true,
+                        )
+                    );
                 }
             }
         }
-        dd('fim');
     }
 
 
@@ -61,6 +72,7 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        DB::table('videos')->truncate();
+        DB::table('video_slugs')->truncate();
     }
 };
