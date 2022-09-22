0da82ec69347cd89aca4e6bdc83581bd55404439
João Pedro <jpedrobs@hotmail.com>
2022-09-22 15:53:53 +0000

    galeria_de_videos

--- database/migrations/2022_09_20_133528_galeria_de_videos.php
+++ database/migrations/2022_09_20_133528_galeria_de_videos.php
@@ -20,24 +20,33 @@ return new class extends Migration
         );
 
         foreach ($posts as $post) {
-            ($post_contents = explode("[", $post->post_content));
+            dump($post_contents = explode("[", $post->post_content));
 
             foreach ($post_contents as $post_content) {
                 $vc_video = explode('-->', preg_replace('/link="(.*)"]/', '$0 --> $1', $post_content));
                 if (count($vc_video) > 1) {
                     $link = trim($vc_video[1]);
                     $link = explode('"', $link);
-                    $link[0];
 
                     $attachment = DB::connection('mysql')->select(
                         'SELECT * from wp_posts p
                     where p.post_type = "attachment"
-                    and p.post_parent = '.$post->id. ' order by id desc limit 1');
-                    
-                    dump($attachment);
+                    and p.post_parent = ' . $post->id . ' order by id desc limit 1'
+                    );
 
+                    /*
+                    $videoID = DB::table('videos')->insertGetId(
+                        array(
+                            'created_at' => $post->post_date,
+                            'updated_at' => $post->post_date,
+                            'link' => $link[0],
+                            'title' => $post->post_title,
+                            'text' => 
+                        )
+                    );*/
+                    //title, link, text e position
+                    //deleted_at, created_at, updated_at, published, publish_start_date e publish_end_date
 
-                    
                 }
             }
         }
