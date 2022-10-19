2e410f7399fb3c6d3fb9ccb83beb3d69604c5046
marcoscoe <marcoscoe9@gmail.com>
2022-10-19 19:28:35 +0000

    swiper - galeria de fotos

--- resources/views/photos/show.blade.php
+++ resources/views/photos/show.blade.php
@@ -1,6 +1,99 @@
 @extends('layouts.layout')
 @section('main')
     <main>
+        <style>
+            html,
+            body {
+                position: relative;
+                height: 100%;
+            }
+            body {
+        background: #eee;
+        font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+        font-size: 14px;
+        color: #000;
+        margin: 0;
+        padding: 0;
+      }
+
+
+            .swiper {
+                width: 100%;
+                height: 100%;
+            }
+
+            .swiper-slide {
+                text-align: center;
+                font-size: 18px;
+                background: #fff;
+
+                /* Center slide text vertically */
+                display: -webkit-box;
+                display: -ms-flexbox;
+                display: -webkit-flex;
+                display: flex;
+                -webkit-box-pack: center;
+                -ms-flex-pack: center;
+                -webkit-justify-content: center;
+                justify-content: center;
+                -webkit-box-align: center;
+                -ms-flex-align: center;
+                -webkit-align-items: center;
+                align-items: center;
+            }
+
+            .swiper-slide img {
+                display: block;
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+            }
+
+            body {
+        background: #fff;
+        color: #000;
+      }
+
+            .swiper {
+                width: 100%;
+                height: 300px;
+                margin-left: auto;
+                margin-right: auto;
+            }
+
+            .swiper-slide {
+                background-size: cover;
+                background-position: center;
+            }
+
+            .mySwiper2 {
+                height: 80%;
+                width: 100%;
+            }
+
+            .mySwiper {
+                height: 20%;
+                box-sizing: border-box;
+                padding: 10px 0;
+            }
+
+            .mySwiper .swiper-slide {
+                width: 25%;
+                height: 100%;
+                opacity: 0.4;
+            }
+
+            .mySwiper .swiper-slide-thumb-active {
+                opacity: 1;
+            }
+
+            .swiper-slide img {
+                display: block;
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+            }
+        </style>
         <div class="header-banner-wrap">
             <div class="essence-banner-image bg-header">
 
@@ -14,13 +107,13 @@
                     <nav aria-label="Breadcrumbs" class="breadcrumb-trail breadcrumbs">
                         <ul class="trail-items" itemscope="" itemtype="http://schema.org/BreadcrumbList">
                             <li class="trail-item trail-begin">
-                                <a href="{{route('home')}}" rel="home">
+                                <a href="{{ route('home') }}" rel="home">
                                     <span>Home</span>
                                 </a>
 
                             </li>
                             <li class="trail-item trail-begin">
-                                <a href="{{route('photos')}}">
+                                <a href="{{ route('photos') }}">
                                     <span>Galeria de Fotos</span>
                                 </a>
 
@@ -35,22 +128,56 @@
             </div>
         </div>
 
-        <div class="container page-content mb-5">
-
+        <div class="container-fluid mb-5">
             <h4 class="text-center">{!! $photo->text !!}</h4>
             <h6 class="text-center">Créditos: {{ $photo->credits }}</h6>
-
-            <div class="pt-4 row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-
-                @foreach ($photo->images('slideshow', 'default') as $image)
-                    <div class="col">
-                        <img src="{{ $image }}" class="card-img-top">
-                    </div>
-                @endforeach
+            <div style="--swiper-navigation-color: #fff; --swiper-pagination-color: #fff" class="swiper mySwiper2">
+                <div class="swiper-wrapper">
+                    @foreach ($photo->images('slideshow', 'default') as $image)
+                        <div class="swiper-slide">
+                            <img src="{{ $image }}" />
+                        </div>
+                    @endforeach
+                </div>
+                <div class="swiper-button-next"></div>
+                <div class="swiper-button-prev"></div>
             </div>
 
+            <div thumbsSlider="" class="swiper mySwiper">
+                <div class="swiper-wrapper">
+                    @foreach ($photo->images('slideshow', 'default') as $image)
+                        <div class="swiper-slide">
+                            <img src="{{ $image }}" />
+                        </div>
+                    @endforeach
+                </div>
 
+            </div>
         </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
+
+        <!-- Initialize Swiper -->
+        <script>
+            var swiper = new Swiper(".mySwiper", {
+                loop: true,
+                spaceBetween: 10,
+                slidesPerView: 4,
+                freeMode: true,
+                watchSlidesProgress: true,
+            });
+            var swiper2 = new Swiper(".mySwiper2", {
+                loop: true,
+                spaceBetween: 10,
+                navigation: {
+                    nextEl: ".swiper-button-next",
+                    prevEl: ".swiper-button-prev",
+                },
+                thumbs: {
+                    swiper: swiper,
+                },
+            });
+        </script>
 
     </main>
 @endsection
