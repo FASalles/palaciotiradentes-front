cb51097da9b2849e7af175e622a993db98a645fa
marcoscoe <marcoscoe9@gmail.com>
2022-10-20 19:25:40 +0000

    banner

--- resources/views/home/partials/banner.blade.php
+++ resources/views/home/partials/banner.blade.php
@@ -17,10 +17,12 @@
 
 <div class="container-fluid p-0">
 
-    <div class="swiper swiper-container mySwiper mb-4">
+    <!-- START-DESKTOP !-->
+    <div class="swiper swiper-container mySwiper mb-4 d-none d-md-block">
         <div class="swiper-wrapper">
             @foreach ($banners as $banner)
-                <div class="swiper-slide swiper-slide-home" style="background-image: url({{ $banner->image('cover', 'default') }});">
+                <div class="swiper-slide swiper-slide-home"
+                    style="background-image: url({{ $banner->image('cover', 'default') }});">
                     <div class="col-12">
                         {!! $banner->text !!}
                         @if (isset($banner->button_text))
@@ -36,6 +38,31 @@
         <div class="swiper-button-prev"></div>
         <div class="swiper-pagination"></div>
     </div>
+    <!-- END-DESKTOP !-->
+
+    
+    <!-- START-MOBILE !-->
+    <div class="swiper swiper-container mySwiper d-md-none">
+        <div class="swiper-wrapper">
+            @foreach ($banners as $banner)
+                <div class="swiper-slide swiper-slide-home"
+                    style="background-image: url({{ $banner->image('cover', 'mobile') }});">
+                    <div class="col-12">
+                        {!! $banner->text !!}
+                        @if (isset($banner->button_text))
+                            <button class="btn btn-md btn-primary">
+                                <a class="text-decoration-none"
+                                    href="{{ $banner->button_link }}">{{ $banner->button_text }}</a></button>
+                        @endif
+                    </div>
+                </div>
+            @endforeach
+        </div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-pagination"></div>
+    </div>
+    <!-- END-MOBILE !-->
 
 
 </div>
