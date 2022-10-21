2691c4f5d09ae153d13c30b361a4e70430df074d
Breno Trengrouse <brenot@gmail.com>
2022-10-21 11:19:05 -0300

    exibitions - mobile - adjusts responsive

--- resources/views/livewire/exhibitions-filter.blade.php
+++ resources/views/livewire/exhibitions-filter.blade.php
@@ -12,14 +12,14 @@
             @foreach ($exhibitions as $exhibition)
                 <div class="col-lg-6">
                     <div class="row mt-4 g-0 border-bottom overflow-hidden pb-4">
-                        <div class="col-4">
+                        <div class="col-12 col-md-4">
                             <img class="card-img-right flex-auto img-fluid" data-src="holder.js/200x250?theme=thumb"
                                  alt="Thumbnail [200x250]" src="{{ $exhibition->image('cover', 'mobile') }}"
                                  data-holder-rendered="true">
                         </div>
-                        <div class="col-8 px-4 d-flex flex-column">
+                        <div class="col-12 col-md-8 px-4 d-flex flex-column mt-3 mt-md-0">
                             <h3 class="mb-2">{{ $exhibition->title }}</h3>
-                            <h5 class="mb-2">DATA: <br>
+                            <h5 class="mb-4 mb-md-2">DATA: <br>
                                 {{ date('d/m/Y H:i', strtotime($exhibition->event_date)) }}
                             </h5>
                             <div class="mt-auto d-flex justify-content-end">
