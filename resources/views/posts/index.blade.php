3a4df3a14c2631211148dc493f8581e65bb40870
marcoscoe <marcoscoe9@gmail.com>
2023-06-05 20:32:40 +0000

    banner and blog

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -17,7 +17,7 @@
                     <nav aria-label="Breadcrumbs" class="breadcrumb-trail breadcrumbs">
                         <ul class="trail-items" itemscope="" itemtype="http://schema.org/BreadcrumbList">
                             <li class="trail-item trail-begin">
-                                <a href="{{route('home')}}" rel="home">
+                                <a href="{{ route('home') }}" rel="home">
                                     <span>Home</span>
                                 </a>
 
@@ -36,7 +36,7 @@
 
         <section class="">
             <div class="container-fluid">
-                <div class="row">
+                <div class="row d-flex justify-content-center">
                     <div class="col-12">
                         <div
                             class="mt-5 vc_separator wpb_content_element vc_separator_align_center vc_sep_width_100 vc_sep_dotted vc_sep_pos_align_center vc_sep_color_grey vc_separator-has-text">
@@ -47,164 +47,47 @@
                         </div>
                     </div>
 
-{{--
-
-                    <div class="row featured-news mt-4">
-
-                        <div class="col-12 col-md-4">
-                            <div class="featured-news-item">
-                                <a class="" href="http://www.palaciotiradentes.rj.gov.br/romanos/">
-                                    <figure>
-                                        <img src="http://www.palaciotiradentes.rj.gov.br/wp-content/uploads/2016/04/13-569x340.jpg" alt="" class="img-fluid">
-                                    </figure>
-                                    <span class=""></span>
-                                </a>
-                                <div class="featured-news-item-info">
-                                    <h4 class="item-title">
-                                        <a href="http://www.palaciotiradentes.rj.gov.br/romanos/">Descubra de onde vieram os símbolos que já deram o que falar</a>
-                                    </h4>
-
-                                    <ul class="text-center">
-                                        <li class="byline post-by"> Por
-                                            <span class="author vcard">
-                                                <a class="url fn n" href="http://www.palaciotiradentes.rj.gov.br/author/admin/">
-                                                    Redação Alerj
-                                                </a>
-                                            </span>
-                                        </li>
-                                        <li class="posted-on post-date">
-                                            <a href="http://www.palaciotiradentes.rj.gov.br/romanos/" rel="bookmark">1 de maio de 2016</a>
-                                        </li>
-                                    </ul>
-
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-12 col-md-4">
-                            <div class="featured-news-item">
-                                <a class="" href="http://www.palaciotiradentes.rj.gov.br/romanos/">
-                                    <figure>
-                                        <img src="http://www.palaciotiradentes.rj.gov.br/wp-content/uploads/2016/04/13-569x340.jpg" alt="" class="img-fluid">
-                                    </figure>
-                                    <span class=""></span>
-                                </a>
-                                <div class="featured-news-item-info">
-                                    <h4 class="item-title">
-                                        <a href="http://www.palaciotiradentes.rj.gov.br/romanos/">Descubra de onde vieram os símbolos que já deram o que falar</a>
-                                    </h4>
-
-                                    <ul class="text-center">
-                                        <li class="byline post-by"> Por
-                                            <span class="author vcard">
-                                                <a class="url fn n" href="http://www.palaciotiradentes.rj.gov.br/author/admin/">
-                                                    Redação Alerj
-                                                </a>
-                                            </span>
-                                        </li>
-                                        <li class="posted-on post-date">
-                                            <a href="http://www.palaciotiradentes.rj.gov.br/romanos/" rel="bookmark">1 de maio de 2016</a>
-                                        </li>
-                                    </ul>
-
-                                </div>
-                            </div>
-                        </div>
-
-
-                        <div class="col-12 col-md-4">
-                            <div class="featured-news-item">
-                                <a class="" href="http://www.palaciotiradentes.rj.gov.br/romanos/">
-                                    <figure>
-                                        <img src="http://www.palaciotiradentes.rj.gov.br/wp-content/uploads/2016/04/13-569x340.jpg" alt="" class="img-fluid">
-                                    </figure>
-                                    <span class=""></span>
-                                </a>
-                                <div class="featured-news-item-info">
-                                    <h4 class="item-title">
-                                        <a href="http://www.palaciotiradentes.rj.gov.br/romanos/">Descubra de onde vieram os símbolos que já deram o que falar</a>
-                                    </h4>
-
-                                    <ul class="text-center">
-                                        <li class="byline post-by"> Por
-                                            <span class="author vcard">
-                                                <a class="url fn n" href="http://www.palaciotiradentes.rj.gov.br/author/admin/">
-                                                    Redação Alerj
-                                                </a>
-                                            </span>
-                                        </li>
-                                        <li class="posted-on post-date">
-                                            <a href="http://www.palaciotiradentes.rj.gov.br/romanos/" rel="bookmark">1 de maio de 2016</a>
-                                        </li>
-                                    </ul>
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
---}}
-
 
                     <div class="row featured-news mt-4">
 
                         @foreach ($featuredPosts as $featuredPost)
-                        <div class="col-12 col-md-4">
-                            <div class="featured-news-item">
-                                <a class="" href="{{ route('posts.show', ['slug' => $featuredPost->slug]) }}">
-                                    <figure>
-                                        <img src="{{ $featuredPost->image('cover', 'mobile') }}" alt="" class="img-fluid">
-                                    </figure>
-                                    <span class=""></span>
-                                </a>
-                                <div class="featured-news-item-info">
-                                    <h4 class="item-title">
-                                        <a href="{{ route('posts.show', ['slug' => $featuredPost->slug]) }}">
-                                            {{ $featuredPost->title }}
-                                        </a>
-                                    </h4>
+                            <div class="col-12 col-xl-4">
+                                <div class="featured-news-item">
+                                    <a class="" href="{{ route('posts.show', ['slug' => $featuredPost->slug]) }}">
+                                        <figure class="image-container">
+                                            <img src="{{ $featuredPost->image('cover', 'default') }}" alt=""
+                                                class="img-fluid full-width">
+                                        </figure>
+                                        <span class=""></span>
+                                    </a>
+                                    <div class="featured-news-item-info">
+                                        <h4 class="item-title">
+                                            <a href="{{ route('posts.show', ['slug' => $featuredPost->slug]) }}">
+                                                {{ $featuredPost->title }}
+                                            </a>
+                                        </h4>
 
-                                    <ul class="text-center">
-                                        <li class="byline post-by"> Por
-                                            <span class="author vcard">
-                                                <a class="url fn n" href="http://www.palaciotiradentes.rj.gov.br/author/admin/">
-                                                    Redação Alerj
-                                                </a>
-                                            </span>
-                                        </li>
-                                        <li class="posted-on post-date">
-                                            <a href="http://www.palaciotiradentes.rj.gov.br/romanos/" rel="bookmark">{{ date("d/m/Y", strtotime($featuredPost->publish_start_date)) }}</a>
-                                        </li>
-                                    </ul>
+                                        <ul class="text-center">
+                                            <li class="byline post-by"> Por
+                                                <span class="author vcard">
+                                                    <a class="url fn n"
+                                                        href="http://www.palaciotiradentes.rj.gov.br/author/admin/">
+                                                        Redação Alerj
+                                                    </a>
+                                                </span>
+                                            </li>
+                                            <li class="posted-on post-date">
+                                                <a href="http://www.palaciotiradentes.rj.gov.br/romanos/"
+                                                    rel="bookmark">{{ date('d/m/Y', strtotime($featuredPost->publish_start_date)) }}</a>
+                                            </li>
+                                        </ul>
 
-                                </div>
-                            </div>
-                        </div>
-                        @endforeach
-                    </div>
-
-
-{{--
-
-                @foreach ($featuredPosts as $featuredPost)
-                            <div class="col-md-4">
-                                <div class="row mt-2 g-0 border-bottom overflow-hidden pb-2">
-                                    <div class="col p-4">
-                                        <div class="mb-1 text-muted">{{ date("d/m/Y", strtotime($featuredPost->publish_start_date)) }}
-                                        </div>
-                                        <h3 class="mb-2">{{ $featuredPost->title }}</h3>
-                                        <a href="{{ route('posts.show', ['slug' => $featuredPost->slug]) }}"
-                                           class="btn btn-bricks mt-2">Leia Mais</a>
                                     </div>
-                                    <img class="card-img-right flex-auto d-none d-md-block img-fluid"
-                                         data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]"
-                                         src="{{ $featuredPost->image('cover', 'mobile') }}" data-holder-rendered="true">
                                 </div>
                             </div>
                         @endforeach
                     </div>
 
---}}
 
                 </div>
             </div>
@@ -215,7 +98,7 @@
                         <div
                             class="mt-5 vc_separator wpb_content_element vc_separator_align_center vc_sep_width_100 vc_sep_dotted vc_sep_pos_align_center vc_sep_color_grey vc_separator-has-text">
                             <span class="vc_sep_holder vc_sep_holder_l"><span class="vc_sep_line"></span></span>
-                            <h4>Últimas Notícias </h4>
+                            <h4>Todas as Notícias </h4>
 
                             <span class="vc_sep_holder vc_sep_holder_r"><span class="vc_sep_line"></span></span>
                         </div>
