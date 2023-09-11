228ce7a086b0d88be4d36af9ae46be462be45032
Breno Trengrouse <brenot@gmail.com>
2023-09-11 14:00:00 -0300

    Change Palacio Tiradentes Text

--- resources/views/history/index.blade.php
+++ resources/views/history/index.blade.php
@@ -38,6 +38,98 @@
             </div>
 
             <div class="row">
+                <div class="col texto-3-colunas">
+                    <p>
+                        O terreno onde está o <span class="fw-bold">Palácio Tiradentes</span>, sede histórica da Assembleia Legislativa do Estado do Rio de Janeiro (Alerj), é um sítio histórico que guarda – desde os tempos do Brasil Colônia – grande parte da memória política do Brasil. O primeiro edifício ali construído foi por volta de  1640 e serviu para abrigar os primeiros três vereadores eleitos. O voto era indireto e o mandato durava apenas um ano. Cabia a eles cuidar da cidade e das finanças públicas. Todo o dinheiro arrecadado ficava guardado em um cofre chamado “burra”, que só podia ser aberto por três chaves: cada uma ficava com um vereador.
+                    </p>
+
+                    <p>
+                        No andar abaixo do cofre público ficava a cadeia da cidade que ficou conhecida como a “Cadeia Velha” ou “Cadeia da Relação”. Foi lá onde o alferes Joaquim José da Silva Xavier – Tiradentes, ficou preso durante três dias antes de ser enforcado em 21 de abril de 1792.
+                    </p>
+                    <p>
+                        Preso de 10 de maio de 1789, numa casa na Rua dos Latoeiros, atual Rua Gonçalves Dias, Tiradentes foi imediatamente levado para uma fortaleza na Ilha das Cobras, permanecendo ali quase três anos, exatos 1.072 dias. Saiu em 17 de abril de 1792, para a “Cadeia da Relação'', demolida para dar  lugar à Câmara dos Deputados, e depois, ao <span class="fw-bold">Palácio Tiradentes</span>. Somente em 1922, já em situação muito precária, a edificação da velha cadeia é demolida para dar lugar a um grande palácio, cuja arquitetura lembra muito o Grand Palais de Paris.
+                    </p>
+
+                    <p>
+                        Projetado em estilo eclético por Archimedes Memória e Francisco Couchet o <span class="fw-bold">Palácio Tiradentes</span> é inaugurado em 6 de maio de 1926, e, passa a abrigar a Câmara federal que ali funcionou de 1926 a 1960, tendo recebido para a posse todos os presidentes do período: de Washington Luiz a Juscelino Kubitscheck.
+                    </p>
+                    <p>
+                        De 1937 a 1945, durante o Estado Novo, o Parlamento foi fechado pelo presidente Getúlio Vargas e no <span class="fw-bold">Palácio Tiradentes</span> passou a funcionar o Ministério da Justiça e o Departamento de Imprensa e Propaganda (DIP), órgão de censura do regime.
+                    </p>
+                    <p>
+                        Em 1960, quando da transferência da capital do país para Brasília, o <span class="fw-bold">Palácio Tiradentes</span> abriga a Assembleia Legislativa do Estado da Guanabara (ALEG), que 15 anos depois, com a fusão dos estados da Guanabara e do Rio de Janeiro, passa a se chamar Assembleia Legislativa do Estado do Rio de Janeiro (ALERJ).
+                    </p>
+
+                    <p>
+                        Hoje em dia, detalhes dessa rica história podem ser conhecidos através de uma Visita Guiada onde o visitante pode acompanhar passo a passo os acontecimentos que marcaram o surgimento da suntuosa edificação e descobrir que não está apenas caminhando por um importante marco arquitetônico carioca. O visitante estará conhecendo o mais importante referencial da democracia brasileira.
+                    </p>
+                    <p>
+                        As escadarias do <span class="fw-bold">Palácio Tiradentes</span> não só abrigaram manifestações políticas e de protestos. Ao longo dos últimos anos, desde a concepção da do projeto da visita guiada (1998), esteve à disposição do público como palco para espetáculos culturais como danças, arte dramática e música.
+                    </p>
+                    <p>
+                        Do mesmo modo, suas galerias, corredores, o plenário e o salão nobre estão abertos aos visitantes, turistas – nacionais e estrangeiros – que encontram no ali um espaço alternativo para a expressão da arte e do entretenimento.
+                    </p>
+                    <p class="fw-bold">
+                        Visite-o!
+                    </p>
+
+                    <div class="small">
+                        Fonte: Herói Brasileiro: A história dos últimos dias de Tiradentes. Robson Pereira/20.04.20100
+                    </div>
+                </div>
+            </div>
+
+
+            {{--<div class="row row-cols-3">
+                <div class="col">
+                    <p>
+                        O terreno onde está o Palácio Tiradentes, sede histórica da Assembleia Legislativa do Estado do Rio de Janeiro (Alerj), é um sítio histórico que guarda – desde os tempos do Brasil Colônia – grande parte da memória política do Brasil. O primeiro edifício ali construído foi por volta de  1640 e serviu para abrigar os primeiros três vereadores eleitos. O voto era indireto e o mandato durava apenas um ano. Cabia a eles cuidar da cidade e das finanças públicas. Todo o dinheiro arrecadado ficava guardado em um cofre chamado “burra”, que só podia ser aberto por três chaves: cada uma ficava com um vereador.
+                    </p>
+
+                    <p>
+                        No andar abaixo do cofre público ficava a cadeia da cidade que ficou conhecida como a “Cadeia Velha” ou “Cadeia da Relação”. Foi lá onde o alferes Joaquim José da Silva Xavier – Tiradentes, ficou preso durante três dias antes de ser enforcado em 21 de abril de 1792.
+                    </p>
+                    <p>
+                        Preso de 10 de maio de 1789, numa casa na Rua dos Latoeiros, atual Rua Gonçalves Dias, Tiradentes foi imediatamente levado para uma fortaleza na Ilha das Cobras, permanecendo ali quase três anos, exatos 1.072 dias. Saiu em 17 de abril de 1792, para a “Cadeia da Relação'', demolida para dar  lugar à Câmara dos Deputados, e depois, ao Palácio Tiradentes. Somente em 1922, já em situação muito precária, a edificação da velha cadeia é demolida para dar lugar a um grande palácio, cuja arquitetura lembra muito o Grand Palais de Paris.
+                    </p>
+                </div>
+                <div class="col">
+                    <p>
+                        Projetado em estilo eclético por Archimedes Memória e Francisco Couchet o Palácio Tiradentes é inaugurado em 6 de maio de 1926, e, passa a abrigar a Câmara federal que ali funcionou de 1926 a 1960, tendo recebido para a posse todos os presidentes do período: de Washington Luiz a Juscelino Kubitscheck.
+                    </p>
+                    <p>
+                        De 1937 a 1945, durante o Estado Novo, o Parlamento foi fechado pelo presidente Getúlio Vargas e no Palácio Tiradentes passou a funcionar o Ministério da Justiça e o Departamento de Imprensa e Propaganda (DIP), órgão de censura do regime.
+                    </p>
+                    <p>
+                        Em 1960, quando da transferência da capital do país para Brasília, o Palácio Tiradentes abriga a Assembleia Legislativa do Estado da Guanabara (ALEG), que 15 anos depois, com a fusão dos estados da Guanabara e do Rio de Janeiro, passa a se chamar Assembleia Legislativa do Estado do Rio de Janeiro (ALERJ).
+                    </p>
+                </div>
+                <div class="col">
+                    <p>
+                        Hoje em dia, detalhes dessa rica história podem ser conhecidos através de uma Visita Guiada onde o visitante pode acompanhar passo a passo os acontecimentos que marcaram o surgimento da suntuosa edificação e descobrir que não está apenas caminhando por um importante marco arquitetônico carioca. O visitante estará conhecendo o mais importante referencial da democracia brasileira.
+                    </p>
+                    <p>
+                        As escadarias do Palácio Tiradentes não só abrigaram manifestações políticas e de protestos. Ao longo dos últimos anos, desde a concepção da do projeto da visita guiada (1998), esteve à disposição do público como palco para espetáculos culturais como danças, arte dramática e música.
+                    </p>
+                    <p>
+                        Do mesmo modo, suas galerias, corredores, o plenário e o salão nobre estão abertos aos visitantes, turistas – nacionais e estrangeiros – que encontram no ali um espaço alternativo para a expressão da arte e do entretenimento.
+                    </p>
+                    <p>
+                        Visite-o!
+                    </p>
+
+                </div>
+            </div>
+--}}
+
+
+
+
+
+
+
+{{--
+            <div class="row">
                 <div class="col-sm">
                     <div class="wpb_wrapper">
                         <p>
@@ -79,11 +171,60 @@
                         <p class="">
                             Fonte: Herói Brasileiro: A história dos últimos dias de Tiradentes. Robson Pereira/20.04.2010
                         </p>
-
                     </div>
                 </div>
             </div>
+            --}}
+
         </div>
+
+
+
+        <!---------- | Comunicado da Diretoria | ------------>
+        {{--
+        <div class="container page-content">
+            <div class="row mb-2 text-center">
+                <div class="col-md-12 page-title">
+                    <h2>Comunicado da Diretoria</h2>
+                    <h6>Ipsum lorem qua estae</h6>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4">
+                    <p>
+                        <img src="images/foto_diretora_comunicacao.jpg" class="img-fluid">
+                    </p>
+                </div>
+                <div class="col-8 texto-2-colunas ps-5">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nisl ante, lobortis id gravida vitae, faucibus sed velit.
+                        </p>
+                    <p>
+                    Suspendisse consectetur, dui et lacinia finibus, nunc dolor efficitur ligula, ut pharetra sapien odio at risus. Curabitur leo risus, aliquam vel diam nec, sollicitudin placerat tellus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin ornare mauris nec pretium vehicula. Praesent consectetur arcu sit amet felis volutpat, vitae viverra neque aliquet. Nunc volutpat elit vitae tempus maximus.
+                    </p>
+                    <p>
+                        Duis sapien est, posuere maximus semper vitae, cursus in dolor. Morbi congue leo a posuere rhoncus. Etiam enim risus, suscipit a rhoncus a, eleifend quis massa. Nulla suscipit libero dui, quis finibus justo efficitur id. Nam faucibus ligula at aliquet venenatis.
+                    </p>
+                    <p>
+                        Ut a mauris ac dui porta mollis. Maecenas vel feugiat tortor, sed tristique ante. Ut luctus consequat diam, non fringilla magna ultrices a. Proin vitae lectus eget nulla elementum congue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras sed mi justo.
+                    </p>
+                    <p>
+                        Quisque faucibus arcu tellus, a finibus dolor sagittis non. Donec id euismod nunc. Morbi quis lorem at nisl gravida ultricies. Mauris eget dignissim quam, id congue nunc.
+                    </p>
+                    <p>
+                        Proin ultricies, diam sit amet auctor semper, nisl lorem aliquet nibh, et porttitor nulla magna eu augue. Phasellus mattis eu mi non aliquet. Morbi vitae eros luctus, iaculis orci sit amet, interdum mauris.
+                    </p>
+                    <p>
+                        Nam elementum porta consectetur. Morbi finibus vel arcu eu dapibus. Proin eget tortor a dui aliquam vehicula id convallis leo. Maecenas et turpis sem. Phasellus malesuada pharetra metus, id vulputate felis sodales quis. Nullam sem sem, rutrum eget gravida sed, cursus sed mi.</p>
+                    <p>
+                        Morbi dolor enim, ullamcorper et fermentum in, maximus mattis nibh. Nullam ac imperdiet lorem, vel condimentum elit.
+                    </p>
+                </div>
+            </div>
+        </div>
+        --}}
+        <!---------- | FIM - Comunicado da Diretoria | ------------>
+
 
         <div class="container-fluid mt-5">
             <div class="row">
