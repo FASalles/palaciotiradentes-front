9f3df37396b633ee24b562e180e3010f55a2b58f
Breno Trengrouse <brenot@gmail.com>
2023-09-25 13:38:16 -0300

    change agende uma visita text

--- resources/views/guided-tour/index.blade.php
+++ resources/views/guided-tour/index.blade.php
@@ -76,55 +76,35 @@ O acesso para cadeirantes é pela Rua Dom Manuel, s/nº, atrás do palácio.</sp
                 </div>
             </div>
 
-
             <div class="row">
-                <div class="col-sm">
-                    <div class="wpb_wrapper">
-
-                        <p>Visitar os corredores do Palácio Tiradentes é fazer um passeio pela tradição parlamentar
-                            brasileira. Graças à exposição permanente “Palácio Tiradentes: Lugar de Memória do Parlamento
-                            Brasileiro”, o público pode acompanhar passo a passo todos os acontecimentos importantes que
-                            marcaram a existência do Palácio. O visitante vai descobrir que não está apenas caminhando por
-                            um dos mais importantes marcos arquitetônicos da “Belle Époque” carioca, mas também pelo mais
-                            importante referencial da democracia brasileira.<u></u>&nbsp;<u></u></p>
-                        <p>A cada passo, a&nbsp;<em>viagem&nbsp;</em>pela história prossegue, conduzindo o público até a
-                            abertura da Assembleia Geral Legislativa do Império, em 1826, quando o edifício colonial onde
-                            funcionava a cadeia pública, mais tarde denominada de Cadeia Velha, foi escolhido para ser a
-                            sede da Câmara dos Deputados.<u></u></p>
-
-                    </div>
-                </div>
-                <div class="col-sm">
-                    <div class="wpb_wrapper">
-                        <p>Durante o&nbsp;<em>passeio</em>&nbsp;é até possível sentir o clima solene e imaginar os deputados
-                            reunidos em frente ao imperador nessa ocasião memorável. Assim sendo, o tempo vai avançando até
-                            a Proclamação da República (15 nov 1889) e a instalação da nova Câmara dos Deputados, agora
-                            republicana, no prédio da Cadeia Velha.<u></u>&nbsp;<u></u></p>
-                        <p>Chega 1922, onde o visitante acompanha a demolição da cadeia velha e vê surgir a nova sede da
-                            Câmara dos Deputados: o Palácio Tiradentes, inaugurado em noite de gala do dia 06 de maio de
-                            1926 e prestigiado com a visita das personalidades mais ilustres do país.<u></u>&nbsp;<u></u>
-                        </p>
-                        <p>A história do Palácio vai se revelando em detalhes aos olhos do grupo, que assiste, em 1934, o
-                            lugar reafirmando a sua tradição democrática, quando abriga a Assembleia Nacional
-                            Constituinte.<u></u></p>
-                    </div>
-                </div>
-                <div class="col-sm">
-                    <div class="wpb_wrapper">
-                        <p>O&nbsp;visitante é levado a assistir a democracia sofrer um terrível golpe, em 1937, com o
-                            fechamento do Palácio Tiradentes e da Câmara dos Deputados durante a ditadura do presidente
-                            Getúlio Vargas. Mas seguindo os painéis do corredor principal do palácio é possível acompanhar o
-                            fim da tirania Vargas e o retorno triunfal da soberania popular, com a instalação da
-                            Constituinte de 1946.&nbsp;&nbsp;A partir desse ponto, o Palácio Tiradentes passa a ser a sede
-                            da Câmara dos Deputados, condição que mantém até a capital do país se mudar para
-                            Brasília.<u></u>&nbsp;<u></u></p>
-                        <p>O&nbsp;guia conduz o visitante até o surgimento do novo Estado do Rio de Janeiro, em 1975, com a
-                            fusão do estado da Guanabara, quando finalmente o Palácio Tiradentes se transforma na sede da
-                            Alerj, a Casa das leis e da cidadania do povo Fluminense.<u></u></p>
-
-                    </div>
+                <div class="col texto-3-colunas">
+                    <p>
+                        Visitar os corredores do <span class="fw-bold">Palácio Tiradentes</span> é fazer um passeio pela tradição parlamentar brasileira. Graças ao projeto da Visita Guiada, o público pode acompanhar passo a passo todos os acontecimentos importantes que marcaram a existência da edificação inaugurada em 06 de maio de 1926. O visitante vai descobrir que não está apenas caminhando por um respeitável marco arquitetônico da “Belle Époque” carioca, mas também pelo mais importante referencial da democracia brasileira.
+                    </p>
+                    <p>
+                        A cada passo, o tour pela história prossegue, conduzindo o público até a abertura da Assembleia Geral Legislativa do Império, em 1826, quando o edifício colonial onde funcionava a cadeia pública, mais tarde denominada de “Cadeia Velha”, foi escolhido para ser a sede da Câmara dos Deputados. Porém, o tempo parece avançar pelos longos corredores em piso mosaico francês sob as narrações dos guias até a Proclamação da República (15Nov1889), e a instalação da nova Câmara dos Deputados, agora republicana, no prédio da velha cadeia.
+                    </p>
+                    <p>
+                        O ano de 1922 é marcado pela demolição da Cadeia Velha, e ao visitante é apresentada a maquete da suntuosa nova sede da Câmara dos Deputados: o <span class="fw-bold">Palácio Tiradentes</span>, inaugurado em noite de gala no dia 06 de maio de 1926, cuja solenidade foi prestigiada com a visita das personalidades mais ilustres do país.
+                    </p>
+                    <p>
+                        A história do <span class="fw-bold">Palácio Tiradentes</span> vai se revelando em detalhes aos olhos do público atento a narrativa sobre o ano de 1934, onde o lugar que reafirma a sua tradição democrática, abriga a Assembleia Nacional Constituinte.
+                    </p>
+                    <p>
+                        O público é levado a assistir a democracia sofrer um terrível golpe, em 1937, com o fechamento do <span class="fw-bold">Palácio Tiradentes</span> e da Câmara dos Deputados durante a ditadura do presidente Getúlio Vargas.
+                    </p>
+                    <p>
+                        Seguindo a história, passando pelo plenário Barbosa Lima Sobrinho, pela Sala do Café e o Salão Nobre, entre outras dependências, é possível rememorar o fim da tirania Vargas e o retorno triunfal da soberania popular, com a instalação da Constituinte de 1946. A partir desse ponto, o <span class="fw-bold">Palácio Tiradentes</span> passa a ser a sede da Câmara dos Deputados, condição que mantém até a capital do país se mudar para Brasília (21Abr1960).
+                    </p>
+                    <p>
+                        O guia conduz o visitante até o surgimento do novo Estado do Rio de Janeiro, em 1975, com a fusão do estado da Guanabara, quando finalmente o <span class="fw-bold">Palácio Tiradentes</span> se transforma na sede da Assembleia Legislativa do Estado do Rio de Janeiro (Alerj), a Casa das leis e da cidadania do povo Fluminense, até que em 21 de Julho de 2021, os parlamentarem se mudam para o prédio do extinto Banco do Estado do Rio de Janeiro (Banerj), na Rua da Ajuda, nº 5, no Centro.
+                    </p>
                 </div>
             </div>
+
+
+
+
         </div>
 
 
