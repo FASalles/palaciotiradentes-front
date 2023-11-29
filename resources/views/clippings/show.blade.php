0a62b29d497c08feeb3a89e5d5b8bac1c3c9ca8e
marcoscoe <marcoscoe9@gmail.com>
2023-11-29 15:56:10 +0000

    fix issue #113

--- resources/views/clippings/show.blade.php
+++ resources/views/clippings/show.blade.php
@@ -31,74 +31,77 @@
             </div>
         </div>
 
-        <div id="main-container" class="main-container p-5 d-none d-md-block">
-            <div class="site-content-inner container">
+        @if($clipping->clippings)
+            <div id="main-container" class="main-container p-5 d-none d-md-block">
+                <div class="site-content-inner container">
 
-                <div class="row">
-                    <div id="primary" class="content-area col-xs-12 no-sidebar">
-                        <main id="main" class="site-main">
-                            <div class="blog-single">
-                                <article id="post-1659"
-                                         class="blog-item post-1659 essential_grid type-essential_grid status-publish has-post-thumbnail hentry tag-70 essential_grid_category-fevereiro">
-                                    <div class="entry-content content-post">
-                                        <!-- AddThis Sharing Buttons above -->
-                                        <div class="vc_row wpb_row vc_row-fluid">
-                                            <div class="wpb_column vc_column_container vc_col-sm-12">
-                                                <div class="vc_column-inner ">
-                                                    <div class="wpb_wrapper">
+                    <div class="row">
+                        <div id="primary" class="content-area col-xs-12 no-sidebar">
+                            <main id="main" class="site-main">
+                                <div class="blog-single">
+                                    <article id="post-1659"
+                                             class="blog-item post-1659 essential_grid type-essential_grid status-publish has-post-thumbnail hentry tag-70 essential_grid_category-fevereiro">
+                                        <div class="entry-content content-post">
+                                            <!-- AddThis Sharing Buttons above -->
+                                            <div class="vc_row wpb_row vc_row-fluid">
+                                                <div class="wpb_column vc_column_container vc_col-sm-12">
+                                                    <div class="vc_column-inner ">
+                                                        <div class="wpb_wrapper">
 
-                                                        <div class="vc_tta-container" data-vc-action="collapse">
-                                                            <div
-                                                                class="vc_general vc_tta vc_tta-tabs vc_tta-color-grey vc_tta-style-classic vc_tta-shape-rounded vc_tta-spacing-1 vc_tta-tabs-position-left vc_tta-controls-align-left ">
+                                                            <div class="vc_tta-container" data-vc-action="collapse">
+                                                                <div
+                                                                    class="vc_general vc_tta vc_tta-tabs vc_tta-color-grey vc_tta-style-classic vc_tta-shape-rounded vc_tta-spacing-1 vc_tta-tabs-position-left vc_tta-controls-align-left ">
 
-                                                                <div class="vc_tta-tabs-container">
-                                                                    <ul class="vc_tta-tabs-list">
-                                                                        @foreach ($clipping->clippings as $clippingAccordion)
-                                                                            <li class="vc_tta-tab @if ($clippingAccordion['id'] == $clipping->clippings[0]['id']) vc_active @endif"
-                                                                                data-vc-tab=""><a
-                                                                                    href="#{{ $clippingAccordion['id'] }}"
-                                                                                    data-vc-tabs=""
-                                                                                    data-vc-container=".vc_tta"><span
-                                                                                        class="vc_tta-title-text">{{ $clippingAccordion['name'] ?? ''}}
+                                                                    <div class="vc_tta-tabs-container">
+                                                                        <ul class="vc_tta-tabs-list">
+                                                                            @foreach ($clipping->clippings as $clippingAccordion)
+                                                                                <li class="vc_tta-tab @if ($clippingAccordion['id'] == $clipping->clippings[0]['id']) vc_active @endif"
+                                                                                    data-vc-tab=""><a
+                                                                                        href="#{{ $clippingAccordion['id'] }}"
+                                                                                        data-vc-tabs=""
+                                                                                        data-vc-container=".vc_tta"><span
+                                                                                            class="vc_tta-title-text">{{ $clippingAccordion['name'] ?? ''}}
                                                                                     </span></a></li>
-                                                                        @endforeach
-                                                                    </ul>
-                                                                </div>
-                                                                <div class="vc_tta-panels-container">
-                                                                    @foreach ($clipping->clippings as $clippingAccordion)
-                                                                        <div class="vc_tta-panels">
-                                                                            <div
-                                                                                class="vc_tta-panel @if ($clippingAccordion['id'] == $clipping->clippings[0]['id']) vc_active @endif"
-                                                                                id="{{ $clippingAccordion['id'] }}"
-                                                                                data-vc-content=".vc_tta-panel-body">
-                                                                                <!--vc_active!-->
-                                                                                <div class="vc_tta-panel-heading">
-                                                                                    <h4 class="vc_tta-panel-title"><a
-                                                                                            href="#{{ $clippingAccordion['id'] }}"
-                                                                                            data-vc-accordion=""
-                                                                                            data-vc-container=".vc_tta-container"><span
-                                                                                                class="vc_tta-title-text">{{ $clippingAccordion['name'] ?? '' }}</span></a>
-                                                                                    </h4>
-                                                                                </div>
+                                                                            @endforeach
+                                                                        </ul>
+                                                                    </div>
+                                                                    <div class="vc_tta-panels-container">
+                                                                        @foreach ($clipping->clippings as $clippingAccordion)
+                                                                            <div class="vc_tta-panels">
+                                                                                <div
+                                                                                    class="vc_tta-panel @if ($clippingAccordion['id'] == $clipping->clippings[0]['id']) vc_active @endif"
+                                                                                    id="{{ $clippingAccordion['id'] }}"
+                                                                                    data-vc-content=".vc_tta-panel-body">
+                                                                                    <!--vc_active!-->
+                                                                                    <div class="vc_tta-panel-heading">
+                                                                                        <h4 class="vc_tta-panel-title">
+                                                                                            <a
+                                                                                                href="#{{ $clippingAccordion['id'] }}"
+                                                                                                data-vc-accordion=""
+                                                                                                data-vc-container=".vc_tta-container"><span
+                                                                                                    class="vc_tta-title-text">{{ $clippingAccordion['name'] ?? '' }}</span></a>
+                                                                                        </h4>
+                                                                                    </div>
 
-                                                                                <div class="vc_tta-panel-body"
-                                                                                     style="">
-                                                                                    <div
-                                                                                        class="wpb_text_column wpb_content_element ">
-                                                                                        <div class="wpb_wrapper">
-                                                                                            {!! $clippingAccordion['description'] ?? ''!!}
-                                                                                            <br><br>
-                                                                                            <a target="_blank"
-                                                                                               href="{{ $clippingAccordion['link'] ?? ''}}">Confira
-                                                                                                a matéria
-                                                                                                na íntegra</a>
+                                                                                    <div class="vc_tta-panel-body"
+                                                                                         style="">
+                                                                                        <div
+                                                                                            class="wpb_text_column wpb_content_element ">
+                                                                                            <div class="wpb_wrapper">
+                                                                                                {!! $clippingAccordion['description'] ?? ''!!}
+                                                                                                <br><br>
+                                                                                                <a target="_blank"
+                                                                                                   href="{{ $clippingAccordion['link'] ?? ''}}">Confira
+                                                                                                    a matéria
+                                                                                                    na íntegra</a>
+                                                                                            </div>
                                                                                         </div>
                                                                                     </div>
                                                                                 </div>
-                                                                            </div>
 
-                                                                        </div>
-                                                                    @endforeach
+                                                                            </div>
+                                                                        @endforeach
+                                                                    </div>
                                                                 </div>
                                                             </div>
                                                         </div>
@@ -106,55 +109,54 @@
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </article>
-                            </div>
-                        </main>
+                                    </article>
+                                </div>
+                            </main>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="container-fluid d-md-none pt-5 pb-5">
+            <div class="container-fluid d-md-none pt-5 pb-5">
 
-            <div class="accordion" id="accordionExample">
-                @foreach ($clipping->clippings as $clippingAccordion)
-                    <div class="accordion-item">
+                <div class="accordion" id="accordionExample">
+                    @foreach ($clipping->clippings as $clippingAccordion)
+                        <div class="accordion-item">
 
-                        <h2 class="accordion-header" id="heading{{$clippingAccordion['id']}}">
+                            <h2 class="accordion-header" id="heading{{$clippingAccordion['id']}}">
 
-                            <button class="accordion-button collapsed shadow-none" type="button"
-                                    data-bs-toggle="collapse"
-                                    data-bs-target="#collapse{{$clippingAccordion['id']}}" aria-expanded="false"
-                                    aria-controls="collapse{{$clippingAccordion['id']}}">
-                                {{ $clippingAccordion['name'] ?? ''}}
+                                <button class="accordion-button collapsed shadow-none" type="button"
+                                        data-bs-toggle="collapse"
+                                        data-bs-target="#collapse{{$clippingAccordion['id']}}" aria-expanded="false"
+                                        aria-controls="collapse{{$clippingAccordion['id']}}">
+                                    {{ $clippingAccordion['name'] ?? ''}}
 
-                            </button>
+                                </button>
 
-                        </h2>
+                            </h2>
 
 
-                        <div id="collapse{{$clippingAccordion['id']}}"
-                             class="accordion-collapse collapse @if ($clippingAccordion['id'] == $clipping->clippings[0]['id']) show @endif"
-                             aria-labelledby="heading{{$clippingAccordion['id']}}" data-bs-parent="#accordionExample">
-                            <div class="accordion-body">
+                            <div id="collapse{{$clippingAccordion['id']}}"
+                                 class="accordion-collapse collapse @if ($clippingAccordion['id'] == $clipping->clippings[0]['id']) show @endif"
+                                 aria-labelledby="heading{{$clippingAccordion['id']}}"
+                                 data-bs-parent="#accordionExample">
+                                <div class="accordion-body">
 
-                                {!! $clippingAccordion['description'] ?? ''!!}
-                                <br><br>
-                                @if(!empty($clippingAccordion['link']))
-                                    <a href="{{ $clippingAccordion['link'] }}">Confira
-                                        a matéria
-                                        na íntegra</a>
-                                @endif
+                                    {!! $clippingAccordion['description'] ?? ''!!}
+                                    <br><br>
+                                    @if(!empty($clippingAccordion['link']))
+                                        <a href="{{ $clippingAccordion['link'] }}">Confira
+                                            a matéria
+                                            na íntegra</a>
+                                    @endif
+                                </div>
                             </div>
+
                         </div>
-
-                    </div>
-                @endforeach
-
+                    @endforeach
+                </div>
             </div>
-
-        </div>
+        @endif
 
     </main>
 @endsection
